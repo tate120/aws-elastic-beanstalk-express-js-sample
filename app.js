4f29d50e17f2c0a014a4a7685a85cd931c4666f1
Nick Tate <nicktate@Nicks-MacBook-Pro.local>
2021-08-18 19:11:25 -0400

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('COMMIT!'));
+app.get('/', (req, res) => res.send('This is a test deployment of code change using CodePipeline.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
